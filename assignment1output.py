491d9f1c87afd8aef640a125f96d4728efcce8d2
ShashwatRai1 <104203605+ShashwatRai1@users.noreply.github.com>
2022-04-23 16:40:52 +0530

    Add files via upload

---
+++ assignment1output.py
@@ -0,0 +1,40 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Shashwat Rai\AppData\Local\Programs\Python\Python36\assingment1python21109021.py 
+Enter the first number:345
+Enter the second number:356
+Enter the third number:675
+The average of three numbers is: 458.6666666666667
+Enter the gross income:346666
+Enter the number of dependents:3
+The tax is: 65533.200000000004
+Enter the no. of seconds = 346
+minutes =  5
+seconds = 46
+75
+0 --- 0.0   1.0
+15 --- 0.2588   0.9659
+30 --- 0.5   0.866
+45 --- 0.7071   0.7071
+60 --- 0.866   0.5
+75 --- 0.9659   0.2588
+90 --- 1.0   0.0
+105 --- 0.9659   -0.2588
+120 --- 0.866   -0.5
+135 --- 0.7071   -0.7071
+150 --- 0.5   -0.866
+165 --- 0.2588   -0.9659
+180 --- 0.0   -1.0
+195 --- -0.2588   -0.9659
+210 --- -0.5   -0.866
+225 --- -0.7071   -0.7071
+240 --- -0.866   -0.5
+255 --- -0.9659   -0.2588
+270 --- -1.0   -0.0
+285 --- -0.9659   0.2588
+300 --- -0.866   0.5
+315 --- -0.7071   0.7071
+330 --- -0.5   0.866
+345 --- -0.2588   0.9659
+>>> 
