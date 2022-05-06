066537bca97f2b80297c7a2a543b48ba52b03c6c
ShashwatRai1 <104203605+ShashwatRai1@users.noreply.github.com>
2022-05-06 22:06:54 +0530

    Add files via upload

---
+++ assingment2output21109021.py
@@ -0,0 +1,27 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:\Users\Shashwat Rai\AppData\Local\Programs\Python\Python36\assingment2python21109021.py 
+35
+egaugnal evitisnes esac a si nohtyP
+a case sensitive
+Python is object oriented language
+10
+Pythonisacasesensitivelanguage
+Hey,Shashwat Rai Here!
+My SID is 21109021
+I am from Production department and my CGPA is 9.9
+58
+50
+224 40
+14 0
+Enter a String ; name
+yes
+Enter side a of triangle :45
+Enter side b of triangle : 56
+Enter side c of triangle : 67
+yes
+Enter first number 78
+Enter second number 87
+3
+>>> 
