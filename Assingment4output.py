74c4f15b9dfb7099ca74449f0734602549f2cbdb
ShashwatRai1 <104203605+ShashwatRai1@users.noreply.github.com>
2022-05-27 00:48:12 +0530

    Add files via upload

---
+++ Assingment4output.py
@@ -0,0 +1,30 @@
+Python 3.6.5 (v3.6.5:f59c0932b4, Mar 28 2018, 17:00:18) [MSC v.1900 64 bit (AMD64)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: C:/Users/Shashwat Rai/AppData/Local/Programs/Python/Python36/Assingment4.py 
+Enter grade: 67
+B
+Enter year : 2022
+False
+Question 1: 10 X 4 = 56
+Wrong. The answer is 40
+Question 2: 3 X 6 = 18
+Right!
+Question 3: 2 X 8 = 28
+Wrong. The answer is 16
+Question 4: 1 X 9 = 9
+Right!
+Question 5: 5 X 4 = 20
+Right!
+Question 6: 0 X 8 = 0
+Right!
+Question 7: 6 X 4 = 55
+Wrong. The answer is 24
+Question 8: 4 X 3 = 12
+Right!
+Question 9: 1 X 2 = 2
+Right!
+Question 10: 6 X 3 = 5
+Wrong. The answer is 18
+There are a total 177 pieces in the bowl.
+>>> 
