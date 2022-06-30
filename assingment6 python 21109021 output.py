eb49d25066ac9944edb3275e6612ef623763b0bd
ShashwatRai1 <104203605+ShashwatRai1@users.noreply.github.com>
2022-06-30 23:46:34 +0530

    Add files via upload

---
+++ assingment6 python 21109021 output.py
@@ -0,0 +1,34 @@
+Python 3.10.4 (tags/v3.10.4:9d38120, Mar 23 2022, 23:13:41) [MSC v.1929 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+
+============= RESTART: C:/python3.10/assingment6 python 21109021.py ============
+Enter a number: 6
+Perfect
+Enter Word: roor
+Palindrome
+Enter number of rows: 5
+      1 
+     1 1 
+    1 2 1 
+   1 3 3 1 
+  1 4 6 4 1 
+string : The quick brown fox jumps over the lazy dog
+pangram
+Enter Word with Hypen : green-red-yellow-black-white
+black-green-red-white-yellow
+Enter hyphen separated string: green-red-yellow-black-white
+black-green-red-white-yellow
+
+True
+False
+True
+False
+
+Check whether the said classes are subclasses of the built-in object class or not.
+True
+True
+length of array8
+Enter array: -25 -10 -7 -3 2 4 8 10
+[[-10, 2, 8], [-7, -3, 10]]
+Enter brackets({[]})
+Balanced
